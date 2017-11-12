35b0d23858cee7dec534d8195c36f66613e88c20
hkq325800 <hkq325800@163.com>
2017-11-12 23:21:16 +0800

    ready

---
+++ app/src/main/java/mine/kerchin/datastructure/MainActivity.java
@@ -0,0 +1,37 @@
+package mine.kerchin.datastructure;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void arithmetic(View view) {
+        startActivity(new Intent(this, ArithmeticActivity.class));
+    }
+
+    public void line(View view) {
+    }
+
+    public void stack(View view) {
+    }
+
+    public void tree(View view) {
+    }
+
+    public void pic(View view) {
+    }
+
+    public void find(View view) {
+    }
+
+    public void rank(View view) {
+    }
+}
